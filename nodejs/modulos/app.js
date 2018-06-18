42d26478dfa0d552eebd316140876092a9b32098
Nataly Contreras <lapicherjd@gmail.com>
2018-06-18 11:50:06 -0500

    ejemplos de modulos

---
+++ nodejs/modulos/app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var math = require('./lib/operaciones');
+
+console.log("Programa Iniciado");
+
+console.log("La suma es:", math.suma(2,3));
+console.log("La resta es:", math.resta(5,2));
