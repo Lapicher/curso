c6bfe01206d602b4f58c63cba6b016f6e7389492
Nataly Contreras <lapicherjd@gmail.com>
2018-06-20 20:46:22 -0500

    Paso 1. Ejemplo de paso de parametros por url y querystring

--- nodejs/blog/routes/index.js
+++ nodejs/blog/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// ejemplo pasando variables por url.
+router.get('/acerca/:name', function(req, res, next){
+	var nombre = req.params.name;
+	// realizar logica negocio
+	res.send("<h1> Mi nombre es: "+nombre+"</h1>");
+});
+
+// ejemplo pasando variables por querystring
+router.get('/acerca', function(req, res, next){
+	var persona = {
+		name: req.query.name,
+		job : req.query.job
+	}
+	res.render('acerca', persona);
+})
+
+
+
 module.exports = router;
